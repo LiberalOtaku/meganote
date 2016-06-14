c1bf786ef8a953637dfbb129a2286cf8803fbc9b
LiberalOtaku <ledgam3r1279@gmail.com>
2016-06-14 14:15:44 -0400

    Added dependency injection, some add and edit functionality.

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -1,21 +1,39 @@
-angular.module('meganote.notes', [
-  'ui.router'
-])
+(function() {
+  angular.module('meganote.notes', ['ui.router'])
+    .config(notesConfig)
+    .controller('NotesController', NotesController);
 
-.config(function($stateProvider) {
-  $stateProvider
+  notesConfig.$inject = ['$stateProvider'];
+  function notesConfig($stateProvider) {
+    $stateProvider
 
-  .state('notes', {
-    url: '/notes',
-    templateUrl: 'notes/notes.html',
-    controller: 'NotesController'
-  })
+    .state('notes', {
+      url: '/notes',
+      templateUrl: 'notes/notes.html',
+      controller: 'NotesController'
+    })
 
-  .state('notes.form', {
-    url: '/:noteId',
-    templateUrl: 'notes/notes-form.html'
-  });
-})
+    .state('notes.form', {
+      url: '/:noteId',
+      templateUrl: 'notes/notes-form.html'
+    });
+  };
 
-.controller('NotesController', function() {
-});
+  NotesController.$inject = ['$state', '$scope'];
+  function NotesController($state, $scope) {
+    $state.go('notes.form');
+
+    $scope.notes = [];
+    $scope.note = { title: '', body: '' };
+
+    $scope.save = function() {
+      $scope.notes.push($scope.note);
+      $scope.note = { title: '', body: '' };
+    };
+
+    $scope.edit = function(note) {
+      $scope.editing = true;
+      $scope.note = note;
+    };
+  };
+})();
